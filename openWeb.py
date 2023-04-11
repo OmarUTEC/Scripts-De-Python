98eb4d29a05137f5271a41b73ce8e87075d2a5a9
Om@r <javier.chavarria@utec.edu.pe>
2023-04-10 19:13:22 -0500

    adding link github

--- openWeb.py
+++ openWeb.py
@@ -4,3 +4,13 @@ url = "https://chat.openai.com/chat"
 
 # Open URL in the default web browser
 webbrowser.open(url)
+
+
+# abrir github
+
+import webbrowser
+
+url = "https://github.com/"
+
+# Open URL in the default web browser
+webbrowser.open(url)
